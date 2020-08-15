6ccb98c664bd621d8a614e98e8da233e4c8669b4
Clayton Watts <cletusw@gmail.com>
2020-08-14 21:21:23 -0700

    App styles

--- docs/app.js
+++ docs/app.js
@@ -10,11 +10,24 @@ import './player-list.js';
 
 function App() {
   return html`
+    ${styles}
     <h1>Catchphrase</h1>
     <catchphrase-create></catchphrase-create>
     <catchphrase-join></catchphrase-join>
     <catchphrase-player-list></catchphrase-player-list>
   `;
 }
+
+const styles = html`
+  <style>
+    :host {
+      display: block;
+      padding: 16px;
+    }
+    h1 {
+      margin-top: 8px;
+    }
+  </style>
+`;
 
 customElements.define('catchphrase-app', component(App));
